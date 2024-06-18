227e2a3fd0bad0be9fc72be95693fb0b27261635
Agafina <98034773+Agafina@users.noreply.github.com>
2024-06-18 03:35:56 +0100

    added road sign and state collections to db

--- zaapa/backend/controllers/userController.js
+++ zaapa/backend/controllers/userController.js
@@ -2,9 +2,24 @@ const User = require('../models/userModel')
 
 // Register user
 
-const registerUser = (req, res) => {
-    res.json({mssg:"register user set"})
+const registerUser = async(req, res) => {
+    const { name, email, password, phoneNumber } = req.body;
+
+    try {
+       const newUser = new User({
+            name,
+            email,
+            password,
+            phoneNumber
+        });
+
+        await newUser.save();
+        res.status(201).json({ message: 'User created successfully' });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
 }
+
 
 // Login user
 
--- zaapa/backend/server.js
+++ zaapa/backend/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const userRouter = require('./Routes/userRoute');
+const roadSignRouter = require('./Routes/roadSignRoute')
+const roadStateRouter = require('./Routes/roadStateRoute')
 require('dotenv').config();
 
 const app = express();
 app.use(express.json()); // Fix: Call express.json() as a function
 
 app.use('/api/user', userRouter);
+app.use('/api/roadsigns',roadSignRouter)
+app.use('/api/roadstate',roadStateRouter)
 
 mongoose.connect(process.env.URI)
 .then(() => {
